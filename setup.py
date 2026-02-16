9ad4ef7e5a03798c08b22418daa0b0500e0fc2bc
syuririk <songjeff0@g.skku.edu>
2026-02-16 10:37:13 +0000

    0.2.0

--- setup.py
+++ setup.py
@@ -4,22 +4,30 @@ import pathlib
 here = pathlib.Path(__file__).parent.resolve()
 
 with open(here / "requirements.txt") as f:
-    requirements = f.read().splitlines()
+    requirements = [
+        line.strip()
+        for line in f
+        if line.strip() and not line.startswith("#")
+    ]
+
 
 package_names = ["getData", "processData"]
-package_path = []
-for n in package_names:
-    package_path.append(n + ".*")
-
+package_path = [n + ".*" for n in package_names]
 
 setup(
     name="utils",
     version="0.2.0",
-    packages=find_packages(include= package_names + package_path), 
+    packages=find_packages(include=package_names + package_path),
     include_package_data=True,
     install_requires=requirements,
+
+    extras_require={
+        "krx": ["pykrx>=1.0.41"]
+    },
+
     description="utils",
-    long_description=(here / "README.md").read_text(encoding="utf-8") if (here / "README.md").exists() else "",
+    long_description=(here / "README.md").read_text(encoding="utf-8")
+        if (here / "README.md").exists() else "",
     long_description_content_type="text/markdown",
     python_requires=">=3.8",
 )
